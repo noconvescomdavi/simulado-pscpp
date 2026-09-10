c14a53b1dec8b866e1521b3424ef28b1d47ab131
noconvescomdavi <davilol42@gmail.com>
2026-09-10 08:38:43 -0300

    feat(editor): prepara entrada unificada do studio profissional

---
+++ app/admin/editor/EditorStudioEntry.js
@@ -0,0 +1,4 @@
+"use client";
+import EditorStudioBootstrap from "./EditorStudioBootstrap";
+import EditorKeyboardMap from "./EditorKeyboardMap";
+export default function EditorStudioEntry(){return <><EditorStudioBootstrap/><EditorKeyboardMap/></>}
