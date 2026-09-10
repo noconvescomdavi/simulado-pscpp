d015271f286fd4d0d973b4f590d97fca22d6794c
noconvescomdavi <davilol42@gmail.com>
2026-09-10 08:41:19 -0300

    feat(editor): integra studio entry na pagina do editor

---
+++ app/admin/editor/page.new.js
@@ -0,0 +1,19 @@
+import {redirect} from "next/navigation";
+import {getAdmin} from "../../../lib/admin";
+import EditorClient from './EditorClient';
+import EditorInteractionLayer from './EditorInteractionLayer';
+import EditorCoreGuard from './EditorCoreGuard';
+import EditorMaxSuite from './EditorMaxSuite';
+import EditorStudioEntry from './EditorStudioEntry';
+import './editor.css';
+import './editor-max.css';
+
+export const metadata = {
+  title: 'Editor visual | ESTIBORDO',
+  robots: { index: false, follow: false },
+};
+
+export default async function SiteEditorPage(){
+  if(!(await getAdmin("content.manage")))redirect("/admin");
+  return <><EditorClient /><EditorInteractionLayer /><EditorCoreGuard /><EditorMaxSuite /><EditorStudioEntry /></>;
+}
