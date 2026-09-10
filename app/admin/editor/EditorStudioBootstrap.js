758aeee950b9014a439eaa4144644dd523c67571
noconvescomdavi <davilol42@gmail.com>
2026-09-10 08:38:00 -0300

    feat(editor): integra engines profissionais ao studio

---
+++ app/admin/editor/EditorStudioBootstrap.js
@@ -0,0 +1,5 @@
+"use client";
+import EditorCanvasProEngine from "./EditorCanvasProEngine";
+import EditorProfessionalWorkbench from "./EditorProfessionalWorkbench";
+import EditorSystemStatus from "./EditorSystemStatus";
+export default function EditorStudioBootstrap(){return <><EditorCanvasProEngine/><EditorProfessionalWorkbench/><EditorSystemStatus/></>}
