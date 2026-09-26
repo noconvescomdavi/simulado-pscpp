ca54f19563394c89c5c69938faffe4a7065ff1ce
noconvescomdavi <davilol42@gmail.com>
2026-09-25 23:00:19 -0300

    fix(standalone): use static icon asset

--- app/layout.js
+++ app/layout.js
@@ -45,14 +45,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/pwa-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/estibordo/logos/estibordo-logo-header.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="ESTIBORDO" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="theme-color" content="#07141f" />
-        <link rel="apple-touch-startup-image" href="/pwa-splash" />
+
       </head>
       <body className={`${montserrat.variable} ${poppins.variable}`}>
         {children}
