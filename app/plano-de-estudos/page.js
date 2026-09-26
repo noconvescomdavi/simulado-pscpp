c20e32783d74ae7ed06feea16b1d2d2f5def0fe3
noconvescomdavi <davilol42@gmail.com>
2026-09-25 23:23:12 -0300

    fix(standalone): allow static local shell

--- app/plano-de-estudos/page.js
+++ app/plano-de-estudos/page.js
@@ -6,7 +6,6 @@ import StudentHeader from "../components/StudentHeader";
 import PlanClient from "./PlanClient";
 import styles from "./plano.module.css";
 
-export const dynamic="force-dynamic";
 
 export default async function PlanoDeEstudos({searchParams}){
   const session=await getSession();
